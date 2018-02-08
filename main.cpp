c64e7b31fae59e154b9b5692938cc7f568f5a1a8
Maxwell A Palmer <mpalmer16@mail.csuchico.edu>
2018-02-07 17:48:04 -0800

    final version that gives ascii value and tell uppercase vs lowercase

--- main.cpp
+++ main.cpp
@@ -5,20 +5,28 @@ using namespace std;
 
 int main()
 {
-
+  char letter;
   cout<<"What character do you want to know about?\n";
-
+  cin>>letter;
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
-
+  if(letter >= 'A'&&letter <= 'Z')
+  {
+  cout<<letter<<" is an upper case letter!\n";
+  }
   //when user's entry is between a-z...
+  else if (letter >= 'a'&&letter <= 'z')
+  {
   cout<<" is a lower case letter!\n";
-
+  }
   //in all other cases...
+  else
+  {
   cout<<"?! Pssh. What are you talking about?\n";
-  
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  int ascii;
+  ascii = (int)letter;
+  cout<<"The ASCII value is: "<<ascii<<endl;
 
   return 0;
 }
